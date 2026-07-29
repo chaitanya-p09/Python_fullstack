5e909c9613902a3b6655c72c71281b76176067a4
avulasaijahnavi80826@gmail.com <pulagamchaitanya9@gmail.com>
2026-07-29 14:13:42 +0530

    index.html

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ alert("Welcome to NRIIT learning managemet system")
 let heading = document.getElementById("welcome");
 heading.innerHTML="welcome future software engineers"
 console.log("Heading element:",heading)
+let msg=document.getElementById("message")
+msg.innerHTML="javascript is fun"
+console.log("message element:",msg)
+function showmessage(){
+    alert("welcome to NRIIT LEARNING MANAGEMENT SYSTEM")
+}
