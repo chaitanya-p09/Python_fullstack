ea67f4bd785054c46de15a08f7ddb13e3f00d78f
avulasaijahnavi80826@gmail.com <pulagamchaitanya9@gmail.com>
2026-08-05 16:14:19 +0530

    style.css

--- app.py
+++ app.py
@@ -24,9 +24,32 @@ def register():
         gender=request.form["gender"]
         course=request.form["course"]
         return render_template("register.html")
-@app.route('/trainers')
+@app.route('/login',methods=["POST","GET"])
+def login():
+    if request.method=="POST":
+        return render_template("login,html")
+    return render_template("login.html")
+@app.route('api/register',methods=["POST"])
+def api_register():
+    data=request.get_json()
+    email=dat.get("email")
+    if email in users_db:
+        retur jsonify({"status":"error","message":"user already exists with this email!"}),400
+users_db[email]=data
+return jsonify({"status":"success","message":"registration successful!"})      
+
+    
 def trainers():
     return render_template("trainers.html")
 
 if __name__=='__main__':
     app.run(debug=True)
+    @app.route('/api/login',methods=["POST"])
+    def api_login():
+        data=request.get_json()
+        email=data.get("email")
+        password=data.get("password")
+        user=user_db.get(email)
+        if user and user.get("password")==password:
+            return jsonify({"status":"success","message":"login successful!welcome back."})
+        
